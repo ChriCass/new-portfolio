753fbd0a12aeaa9830cd8456525278075150098e
ChriCass <chris_ccc68@outlook.com>
2026-06-18 09:08:00 -0500

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <main className='container mx-auto px-4'>
+      <h1 className='text-4xl font-bold'>hello there</h1>
+    </main>
+  )
+}
+
+export default App
